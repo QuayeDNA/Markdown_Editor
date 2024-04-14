e1a5d5bd4c31f1ffde87815b8e38ae3e2dbf5fe5
QuayeDNA <quayedavid245@gmail.com>
2024-04-14 13:51:47 +0000

    Refactor frontend styling and update dependencies, fix missing imports, and update App component layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,10 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyle } from "./components/utils/themes";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import Editor from "./components/Editor";
-import Preview from "./components/Preview";
+import Editor from "./components/Editor"; // Add missing import
+import Preview from "./components/Preview"; // Add missing import
 
-import "./App.css"
+import "./App.css";
 
 interface AppProps {
   // Define any props if needed
@@ -14,23 +14,25 @@ interface AppProps {
 
 const App: React.FC<AppProps> = () => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
-  const [showEditor, setShowEditor] = useState(true);
+const [previewActive, setPreviewActive] = useState(false);
 
-  const toggleEditor = () => {
-    setShowEditor(!showEditor);
-  }
+const togglePreview = () => {
+  setPreviewActive(!previewActive);
+};
+
+  
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
-  const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'light');
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
 
   useEffect(() => {
-    localStorage.setItem('theme', theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   const [content, setContent] = useState("");
@@ -43,21 +45,31 @@ const App: React.FC<AppProps> = () => {
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyle />
       <div className="relative h-screen flex overflow-hidden">
-        <Sidebar isOpen={sidebarOpen} toggleTheme={toggleTheme} toggleSidebar={toggleSidebar}/>
-        <div className={`flex flex-col w-full transition-transform duration-200 ease-in-out ${sidebarOpen ? "transform translate-x-64" : ""}`}>
+        <Sidebar
+          isOpen={sidebarOpen}
+          toggleTheme={toggleTheme}
+          toggleSidebar={toggleSidebar}
+        />
+        <div
+          className={`flex flex-col w-full transition-transform duration-200 ease-in-out ${
+            sidebarOpen ? "transform translate-x-64" : ""
+          }`}>
           <Header toggleSidebar={toggleSidebar} sidebarOpen={sidebarOpen} />
-          <main className="flex flex-row overflow-auto h-full">
-  <div style={{ display: showEditor ? 'block' : 'none', width: showEditor ? '50%' : '0', overflow: 'auto' }} className="h-full">
+          {/*/////////////////////////////////////////////////////////////////////*/}
+      <main className="main-container">
+  <div className={`editor-container ${previewActive ? 'translate-x-negative' : ''}`}>
     <Editor
       content={content}
       onContentChange={handleContentChange}
-      onPreviewIconClick={toggleEditor}
+      onPreviewIconClick={togglePreview}
     />
   </div>
-  <div style={{ width: showEditor ? '50%' : '100%', overflow: 'auto' }} className="hidden-on-small w-full h-full">
-    <Preview content={content} onPreviewIconClick={toggleEditor} />
+  <div className={`preview-container ${previewActive ? 'translate-x-zero' : ''}`}>
+    <Preview content={content} onPreviewIconClick={togglePreview} />
   </div>
 </main>
+
+          {/*/////////////////////////////////////////////////////////////////////*/}
         </div>
       </div>
     </ThemeProvider>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,12 @@ export default {
         'roboto': ['Roboto', 'sans-serif'],
         'roboto-mono': ['"Roboto Mono"', 'monospace'],
       },
-      
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
     },
   },
   plugins: [],
