2378f1f2c29d1ce0df4451a3887b153cfd21f147
QuayeDNA <quayedavid245@gmail.com>
2024-04-22 14:36:38 +0000

    Improved UX of messageCard by setting background colors for certain operations

--- frontend/src/components/ui/messageCard.tsx
+++ frontend/src/components/ui/messageCard.tsx
@@ -7,6 +7,7 @@ const MessageCard: React.FC = () => {
   const dispatch = useDispatch();
   const showMessage = useSelector((state: RootState) => state.messages.messages.length > 0);
   const messages = useSelector((state: RootState) => state.messages.messages);
+  const messageType = useSelector((state: RootState) => state.document.type);
   const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode);
   
   useEffect(() => {
@@ -25,11 +26,25 @@ const MessageCard: React.FC = () => {
     };
   }, [showMessage, dispatch]);
 
+  // Function to determine background color based on message type
+  const getMessageBackgroundColor = () => {
+    switch (messageType) {
+      case 'delete':
+        return isDarkMode ? 'bg-red-800' : 'bg-red-500';
+      case 'save':
+        return isDarkMode ? 'bg-green-800' : 'bg-green-500';
+      case 'create':
+        return isDarkMode ? 'bg-yellow-800' : 'bg-yellow-500';
+      default:
+        return isDarkMode ? 'bg-dark-1' : 'bg-light';
+    }
+  };
+
  return (
-  <div className={`fixed bottom-4 right-4 z-50 rounded-lg transition-all duration-500 ease-in-out transform ${showMessage ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'} ${isDarkMode ? 'bg-dark-1 text-light-4 border border-light' : 'bg-light text-grey-3 border border-grey-1'}`}>
+  <div className={`fixed bottom-4 right-4 z-50 rounded-lg transition-all duration-500 ease-in-out transform ${showMessage ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'} ${getMessageBackgroundColor()} text-white border border-white`}>
     <div className="rounded-lg shadow-md p-4">
-      {messages.map((message, index) => (
-        <div key={index} className="mb-2">{message}</div>
+      {messages.map((message) => (
+        <div key={message}>{message}</div>
       ))}
     </div>
   </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
